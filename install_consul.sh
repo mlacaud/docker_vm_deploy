ec15908638ef0af9779c973023431378bd75c017
mlacaud <matlaca24@gmail.com>
2015-11-01 16:30:14 +0100

    install consul with config

--- install_consul.sh
+++ install_consul.sh
@@ -6,3 +6,9 @@ sudo wget https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip
 sudo unzip *.zip
 sudo rm *.zip
 sudo mv ./consul /usr/local/bin
+
+sudo mkdir /etc/consul.d/server
+
+echo '{"bootstrap_expect": 1, "server": true,	"data_dir": "/tmp/consul",	"log_level": "INFO",	"enable_syslog": false,	"client_addr": "0.0.0.0"}' > config.json
+
+sudo mv ./config.json /etc/consul.d/server/config.json
