5f8916e7590e76ee466a04263292ac0f055a81c3
mlacaud <matlaca24@gmail.com>
2016-03-21 11:47:46 +0100

    install consul : new adress

--- consul/install_consul.sh
+++ consul/install_consul.sh
@@ -2,7 +2,7 @@
 #Script for Ubuntu 14.04
 
 #sudo cd /usr/local/bin
-sudo wget https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip
+sudo wget https://releases.hashicorp.com/consul/0.6.4/consul_0.6.4_linux_amd64.zip
 sudo unzip *.zip
 sudo rm *.zip
 sudo mv ./consul /usr/local/bin
